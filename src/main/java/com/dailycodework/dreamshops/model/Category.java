027d3d99a8c6ade61d7fe10100bd03a0e17d6f75
Paribesh101@gmail.com <Paribesh101@gmail.com>
2024-11-25 22:49:12 -0600

    Added Changes

---
+++ src/main/java/com/dailycodework/dreamshops/model/Category.java
@@ -0,0 +1,23 @@
+package com.dailycodework.dreamshops.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+}
